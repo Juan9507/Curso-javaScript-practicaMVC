a0a65f8429033d8687a40643e8423373a285c572
juan9507 <juandavidnaranjo75@gmail.com>
2022-06-04 22:53:07 -0500

    Creacion de las barras del juego de ping pong

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /**
- * Juego de Ping Pong de Código Facilito
+ * Juego de Ping Pong de Código Facilito MVC
  *
  * @author [Juan David Rivera Naranjo - juandavidnaranjo75@gmail.com]
  * @version [v1.0.0]
@@ -7,7 +7,7 @@
  */
 
 /**
- * Función anonima para declarar una clase Board
+ * Función anonima para declarar una clase Board (tablero) que seria el modelo
  */
 (function () {
   /**
@@ -21,7 +21,7 @@
     this.height = height; // Alto
     this.playing = false; //Jugando
     this.game_over = false; //Fin del juego
-    this.bars = []; // Barra
+    this.bars = []; // Barra de las orillas del juego
     this.ball = null; // Pelota
   };
 
@@ -32,14 +32,48 @@
     get elements() {
       //Modificar los metodos de las clases
       var elements = this.bars; // Barra laterales del juego
-      elements.push(ball); // Agregar una pelota
+      elements.push(this.ball); // Agregar una pelota
       return elements; // Retorna todos los elementos que hay en el tablero.
     },
   };
 })();
 
 /**
- * Función anonima para declarar una clase BoardView
+ * Función anonima auto ejecutable para declarar una clase Bar la vista
+ */
+(function () {
+  /**
+   *Clase bar para saber la posicion y ancho y el largo
+   * @param {*} x - recibe la cordenada x
+   * @param {*} y - recibe la cordenada y
+   * @param {*} width - recibe el tamaño
+   * @param {*} height - recibe el alto
+   * @param {*} bars - recibe el bars tablero
+   */
+  self.Bar = function (x, y, width, height, board) {
+    //Constructor de la clase
+    this.x = x; //Cordenada x
+    this.y = y; //Cordenada y
+    this.width = width; //Ancho
+    this.height = height; //Alto
+    this.board = board; //Tablero
+
+    this.board.bars.push(this); // Agregando al tablero las barras laterales
+
+    this.kind = "rectangle"; // Atributo para saber lo que se va a dibujar para que canvas sepa que dibuja
+  };
+
+  /**
+   * Modificación del prototipo de la clase para colocar los metodos de la misma
+   */
+  self.Bar.prototype = {
+    //Declaracion de metodos para despues implementarlas
+    down: function () {},
+    up: function () {},
+  };
+})();
+/**
+ * Función anonima para declarar una clase BoardView la vista
  */
 (function () {
   /**
@@ -54,6 +88,34 @@
     this.board = board; // board
     this.ctx = canvas.getContext("2d"); // Contexto para poder dibujar
   };
+
+  self.BoardView.prototype = {
+    draw: function () {
+      for (var index = this.board.elements.length - 1; index >= 0; index--) {
+        var el = this.board.elements[index];
+        draw(this.ctx, el); //Se le pasa el contexto y el elemento que va a dibujar
+      }
+    },
+  };
+
+  /**
+   * Funcion draw para dibujar
+   * @param {*} ctx
+   * @param {*} element
+   */
+  function draw(ctx, element) {
+    if (element != null && element.hasOwnProperty("kind")) {
+      switch (
+        element.kind //dependiendo de lo que traiga el elemento se dibuja
+      ) {
+        case "rectangle":
+          ctx.fillRect(element.x, element.y, element.width, element.height);
+          break;
+        default:
+          break;
+      }
+    }
+  }
 })();
 
 window.addEventListener("load", main);
@@ -63,6 +125,10 @@ window.addEventListener("load", main);
  */
 function main() {
   var board = new Board(800, 400); //Creando un objeto de la clase board
+  var bar = new Bar(20, 100, 40, 100, board); // Creando un objeto de la clase bar
+  var bar2 = new Bar(735, 100, 40, 100, board); // Creando un objeto de la clase bar
   var canvas = document.querySelector("#canvas"); //Seleccionar el elemento canvas
   var board_view = new BoardView(canvas, board); //Creando un objeto de la clase boardView
+
+  board_view.draw();
 }
