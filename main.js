743a0ff37df262a1c7a332bbc356bddef137d605
juan9507 <juandavidnaranjo75@gmail.com>
2022-06-05 00:33:19 -0500

    Creacion de la pelota del juego

--- main.js
+++ main.js
@@ -39,6 +39,28 @@
 })();
 
 /**
+ * clase Ball para crear la pelota
+ */
+(function () {
+  /**
+   * @param {*} x - recibe la cordenada en x
+   * @param {*} y - recibe la cordenada en y
+   * @param {*} radius - recibe el radio de la pelota
+   * @param {*} board - recide el board
+   */
+  self.Ball = function (x, y, radius, board) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.board = board;
+    this.speed_y = 0; //Velocidad en la cordenada y
+    this.speed_x = 3; //Velocidad en la cordenada y
+    board.ball = this; // le decimos a board que la pelota es this, asignamos al objeto ball
+    this.kind = "circle";
+  };
+})();
+
+/**
  * Función anonima auto ejecutable para declarar una clase Bar la vista
  */
 (function () {
@@ -98,11 +120,27 @@
   };
 
   self.BoardView.prototype = {
+    /**
+     * Metodo para limpiar, dibuja un triangulo transparente
+     */
+    clean: function () {
+      this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+    },
+    /**
+     * Metodo para dibujar la barra, recorre el board.elements
+     */
     draw: function () {
       for (var index = this.board.elements.length - 1; index >= 0; index--) {
         var el = this.board.elements[index];
         draw(this.ctx, el); //Se le pasa el contexto y el elemento que va a dibujar
       }
+    },
+    /**
+     * Metodo para iniciar el juego
+     */
+    play: function () {
+      this.clean();
+      this.draw();
     },
   };
 
@@ -112,43 +150,55 @@
    * @param {*} element
    */
   function draw(ctx, element) {
-    if (element != null && element.hasOwnProperty("kind")) {
-      switch (
-        element.kind //dependiendo de lo que traiga el elemento se dibuja
-      ) {
-        case "rectangle":
-          ctx.fillRect(element.x, element.y, element.width, element.height);
-          break;
-        default:
-          break;
-      }
+    //if (element != null && element.hasOwnProperty("kind")) {
+    switch (
+      element.kind //dependiendo de lo que traiga el elemento se dibuja
+    ) {
+      case "rectangle":
+        ctx.fillRect(element.x, element.y, element.width, element.height);
+        break;
+      case "circle":
+        ctx.beginPath();
+        ctx.arc(element.x, element.y, element.radius, 0, 7);
+        ctx.fill();
+        ctx.closePath();
+      default:
+        break;
     }
+    //}
   }
 })();
 
 var board = new Board(800, 400); //Creando un objeto de la clase board
 var bar = new Bar(20, 100, 40, 100, board); // Creando un objeto de la clase bar
-var bar2 = new Bar(735, 100, 40, 100, board); // Creando un objeto de la clase bar
+var bar_2 = new Bar(735, 100, 40, 100, board); // Creando un objeto de la clase bar
 var canvas = document.querySelector("#canvas"); //Seleccionar el elemento canvas
 var board_view = new BoardView(canvas, board); //Creando un objeto de la clase boardView
+var ball = new Ball(350, 100, 10, board);
 
 /**
  * Escuchar cuando pulsan una tecla para saber el evento a lanzar
  */
 document.addEventListener("keydown", function (ev) {
+  ev.preventDefault();
   if (ev.key == "ArrowUp") {
     bar.up();
   } else if (ev.key == "ArrowDown") {
     bar.down();
+  } else if (ev.key == "w") {
+    bar_2.up();
+  } else if (ev.key == "s") {
+    bar_2.down();
   }
   console.log(bar.toString());
 });
 
-window.addEventListener("load", main);
+window.requestAnimationFrame(controller); // los frames del juego
 
 /**
  * Function main para ejecutar todos los elementos
  */
-function main() {
-  board_view.draw();
+function controller() {
+  board_view.play();
+  window.requestAnimationFrame(controller); // los frames del juego
 }
